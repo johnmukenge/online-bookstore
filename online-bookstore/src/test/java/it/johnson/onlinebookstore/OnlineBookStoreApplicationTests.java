9504fec20781f3a427880e8039ca69ff35e9b320
MukengeNJ <MukengeNJ@VT-MukengeNJ.valueteam.com>
2020-12-10 14:39:24 +0100

    Initial commit. created spring boot application and configure the application propeties

---
+++ online-bookstore/src/test/java/it/johnson/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.johnson.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
